9fca5fe85561b8f58b7a2b8cd41e4d4e4effc741
Raffaele Stefanile <raffaele@Raffaeles-MacBook-Pro.local>
2021-04-28 10:48:18 +0200

    test

--- builds.groovy
+++ builds.groovy
@@ -8,3 +8,34 @@ multibranchPipelineJob('Deployments1/example') {
                   }
              }
 }
+multibranchPipelineJob('Deployments2/example') {
+                branchSources {
+                  git {
+                    id('1234567wefewf89') // IMPORTANT: use a constant and unique identifier
+                      remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+                      credentialsId('github-ci')
+                      includes('JENKINS-*')
+                  }
+             }
+}
+multibranchPipelineJob('Deployments3/example') {
+                branchSources {
+                  git {
+                    id('12345678qwqwr9') // IMPORTANT: use a constant and unique identifier
+                      remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+                      credentialsId('github-ci')
+                      includes('JENKINS-*')
+                  }
+             }
+}
+multibranchPipelineJob('Deployments4/example') {
+                branchSources {
+                  git {
+                    id('12345678ewfwefew9') // IMPORTANT: use a constant and unique identifier
+                      remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+                      credentialsId('github-ci')
+                      includes('JENKINS-*')
+                  }
+             }
+}
+
