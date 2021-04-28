a6879b918610c472dfbfa4b7ea0f2189b1bd8350
Raffaele Stefanile <raffaele@Raffaeles-MacBook-Pro.local>
2021-04-28 10:56:03 +0200

    test

--- builds.groovy
+++ builds.groovy
@@ -1,4 +1,4 @@
-multibranchPipelineJob('Deployments1/example') {
+multibranchPipelineJob('Deployments1/example1') {
                 branchSources {
                   git {
                     id('123456789') // IMPORTANT: use a constant and unique identifier
@@ -8,7 +8,7 @@ multibranchPipelineJob('Deployments1/example') {
                   }
              }
 }
-multibranchPipelineJob('Deployments2/example') {
+multibranchPipelineJob('Deployments2/example2') {
                 branchSources {
                   git {
                     id('1234567wefewf89') // IMPORTANT: use a constant and unique identifier
@@ -18,7 +18,7 @@ multibranchPipelineJob('Deployments2/example') {
                   }
              }
 }
-multibranchPipelineJob('Deployments3/example') {
+multibranchPipelineJob('Deployments3/example3') {
                 branchSources {
                   git {
                     id('12345678qwqwr9') // IMPORTANT: use a constant and unique identifier
@@ -28,7 +28,7 @@ multibranchPipelineJob('Deployments3/example') {
                   }
              }
 }
-multibranchPipelineJob('Deployments4/example') {
+multibranchPipelineJob('Deployments4/example4') {
                 branchSources {
                   git {
                     id('12345678ewfwefew9') // IMPORTANT: use a constant and unique identifier
